3b14e34569557b61e4621193de7bfb4dec6f40f8
kanzaahmed <63643134+kanzaahmed@users.noreply.github.com>
2022-01-16 10:24:28 -0800

    Add files via upload

---
+++ secrets/main.js
@@ -0,0 +1,41 @@
+/* eslint-env browser */ 
+// main.js 
+const updateButton = document.querySelector('#update-button') 
+const deleteButton = document.querySelector('#delete-button') 
+const messageDiv = document.querySelector('#message') 
+updateButton.addEventListener('click', _ => { 
+fetch('/students', { 
+method: 'PUT', 
+headers: { 'Content-Type': 'application/json' }, 
+body: JSON.stringify({ 
+name: 'Student', 
+student: 'I find this CRUD app with CI/CD implemented very interesting.' 
+}) 
+}) 
+.then(res => { 
+if (res.ok) return res.json() 
+}) 
+.then(response => { 
+window.location.reload(true) 
+}) }) 
+
+deleteButton.addEventListener('click', _ => { 
+fetch('/students', { 
+method: 'DELETE', 
+headers: { 'Content-Type': 'application/json' }, 
+body: JSON.stringify({ 
+name: 'Student' 
+}) 
+}) 
+.then(res => { 
+if (res.ok) return res.json() 
+}) 
+.then(response => { 
+if (response === 'No student to delete') { 
+messageDiv.textContent = 'No student to delete' 
+} else { 
+window.location.reload(true) 
+} 
+}) 
+.catch(console.error) 
+}) 
